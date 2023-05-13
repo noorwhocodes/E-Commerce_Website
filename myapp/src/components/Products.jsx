982cccbffdd370eb67e12de36f64e2fdc475a32d
Noor Ul Ain Arshad <89426119+noorwhocodes@users.noreply.github.com>
2023-05-13 21:58:16 +0500

    Search functionality added

--- myapp/src/components/Products.jsx
+++ myapp/src/components/Products.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
+import NavigationBar from "./Navbar";
 
 
 
@@ -8,6 +9,9 @@ const Products = () => {
     const [data, setData] = useState([]);
     const [filter, setFilter] = useState(data);
     const [loading, setLoading] = useState(false);
+
+
+
     let componentMounted = true;
 
     useEffect(() => {
@@ -42,34 +46,60 @@ const Products = () => {
         setFilter(updatedList);
     }
 
+    const [searchQuery, setSearchQuery] = useState('');
+
+    const handleSearch = (searchQuery) => {
+        setSearchQuery(searchQuery);
+        const filteredProducts = data.filter((product) =>
+            product.title.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+        setFilter(filteredProducts);
+    };
+
+
     const ShowProducts = () => {
         return (
             <>
                 <div className="buttons">
-                    <button className="btn btn-outline-dark" onClick={()=> setFilter(data)}>All</button>
-                    <button className="btn btn-outline-dark" onClick={()=> filterProduct("women's clothing")}>Women Clothing</button>
-                    <button className="btn btn-outline-dark" onClick={()=> filterProduct("men's clothing")}>Men Clothing</button>
-                    <button className="btn btn-outline-dark"onClick={()=> filterProduct("jewelery")}>Jewelery</button>
+                    <button className="btn btn-outline-dark" onClick={() => setFilter(data)}>All</button>
+                    <button className="btn btn-outline-dark" onClick={() => filterProduct("women's clothing")}>Women Clothing</button>
+                    <button className="btn btn-outline-dark" onClick={() => filterProduct("men's clothing")}>Men Clothing</button>
+                    <button className="btn btn-outline-dark" onClick={() => filterProduct("jewelery")}>Jewelery</button>
                     <hr />
                 </div>
+
+                <div className="d-flex mb-3">
+                    <input
+                        className="form-control me-2"
+                        type="search"
+                        placeholder="Search products"
+                        value={searchQuery}
+                        onChange={(e) => handleSearch(e.target.value)}
+                    />
+                    <button className="btn btn-outline-dark" onClick={() => handleSearch(searchQuery)}>
+                        Search
+                    </button>
+                </div>
+
+
                 {filter.map((product) => {
                     return (
                         <>
                             <div className="col-md-3">
                                 <div class="card h-100 text-center p-4" key={product.id}>
-                                    <img src={product.image} class="card-img-top" 
-                                                    alt={product.title} height="250px"/>
-                                        <div class="card-body">
-                                            <h5 class="card-title mb-0">{product.title}</h5>
-                                            <p class="card-text">{product.description}
-                                            </p>
-                                            <p class="card-text">${product.price}
-                                            </p>
+                                    <img src={product.image} class="card-img-top"
+                                        alt={product.title} height="250px" />
+                                    <div class="card-body">
+                                        <h5 class="card-title mb-0">{product.title}</h5>
+                                        <p class="card-text">{product.description}
+                                        </p>
+                                        <p class="card-text">${product.price}
+                                        </p>
 
-                                            <NavLink key={product.id} to={`/product/${product.id}`}>
-                                                View
-                                                </NavLink>
-                                        </div>
+                                        <NavLink key={product.id} to={`/product/${product.id}`}>
+                                            View
+                                        </NavLink>
+                                    </div>
                                 </div>
 
 
@@ -86,6 +116,7 @@ const Products = () => {
 
     return (
         <div>
+            <NavigationBar />
             <div className="container my-5 py-5">
                 <div className="row1">
                     <div className="col-12 mb-5">
